af3515f7700b6c5fec37b12e03d8e88d6cea18cf
Laveen <lavink32@gmail.com>
2023-08-07 15:47:10 +0530

    f

--- packages/SearchInput/types/SearchInput.d.ts
+++ packages/SearchInput/types/SearchInput.d.ts
@@ -3,4 +3,4 @@ import "./styles.css";
 export type UpdaptSearchProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
     width: number;
 };
-export declare function UpdaptSearchInput({ width }: UpdaptSearchProps): React.JSX.Element;
+export declare function UpdaptSearchInput({ width, disabled, ...rest }: UpdaptSearchProps): React.JSX.Element;
