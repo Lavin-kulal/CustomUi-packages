5cdcd285d98d274441710edcd732e3c40b00352d
Laveen <lavink32@gmail.com>
2023-08-08 13:00:27 +0530

    ss

--- packages/SearchInput/dist/esm/types/SearchInput.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import "./styles.css";
-export type UpdaptSearchProps = Omit<React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, "disabled"> & {
-    width: number;
-    disabled?: boolean;
-};
-export declare function UpdaptSearchInput({ width, disabled, ...rest }: UpdaptSearchProps): React.JSX.Element;
--- packages/SearchInput/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-type UpdaptSearchProps = Omit<React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, "disabled"> & {
-    width: number;
-    disabled?: boolean;
-};
-declare function UpdaptSearchInput({ width, disabled, ...rest }: UpdaptSearchProps): React.JSX.Element;
-
-export { UpdaptSearchInput, UpdaptSearchProps };
