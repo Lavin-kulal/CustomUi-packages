cd5595c2d5febad8c40990760a2b3406a9ed79b6
Laveen <lavink32@gmail.com>
2023-08-08 19:22:36 +0530

    d

--- packages/DatePicker/types/DatePicker.d.ts
+++ packages/DatePicker/types/DatePicker.d.ts
@@ -2,7 +2,6 @@ import * as React from "react";
 import "./styles.css";
 import "react-datepicker/dist/react-datepicker.css";
 export type UpdaptDatePickerProps = {
-    onChange(date: Date | null, event: React.SyntheticEvent<any, Event> | undefined): void;
     width: number;
 };
-export declare function UpdaptDatePicker({ onChange, width }: UpdaptDatePickerProps): React.JSX.Element;
+export declare function UpdaptDatePicker({ width }: UpdaptDatePickerProps): React.JSX.Element;
