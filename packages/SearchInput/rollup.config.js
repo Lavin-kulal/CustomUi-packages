b2b396e0c7cd3bbdae9154bf7f451a2b765004d8
Laveen <lavink32@gmail.com>
2023-08-17 15:01:14 +0530

    a

--- packages/SearchInput/rollup.config.js
+++ packages/SearchInput/rollup.config.js
@@ -1,10 +1,11 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import babel from "@rollup/plugin-babel";
+import babel from "@rollup/plugin-babel"; // Import the Babel plugin
 import replace from "@rollup/plugin-replace";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
+
 export default [
   {
     input: "src/index.ts",
@@ -30,7 +31,7 @@ export default [
       typescript({ tsconfig: "./tsconfig.json" }),
       babel({
         babelHelpers: "bundled",
-        presets: ["@babel/preset-react"],
+        presets: ["@babel/preset-env", "@babel/preset-react"],
         extensions: [".js"],
       }),
       commonjs(),
